f59815e545efae5f7d792d54b33f02a4220a32e0
Dima <dmitro-sv@outlook.com>
2020-09-06 09:01:17 +0300

    sql

--- DataBaseOrganization/lab1/main.c
+++ DataBaseOrganization/lab1/main.c
@@ -9,14 +9,28 @@ struct NodeStruct{
 typedef struct NodeStruct* List;
 typedef struct NodeStruct Node;
 
+
+/// It is my function
+/// \param a ashka
+/// \param b beshka
+/// \param c tseshka
+/// \return abetseshka
+int fooo(int a, int b, int c){
+    return a*b*c;
+}
+
 int main() {
-    List l = malloc(sizeof(Node));
-    l->data = 55;
-    l->next = malloc(sizeof(Node));
-    l->next->data = 66;
-    l->next->next = NULL;
-    for(Node* it = l; it; it=it->next){
-        printf("%d\n", it->data);
-    }
+    printf("%d", fooo(3,5,7));
+//    List l = malloc(sizeof(Node));
+//    l->data = 55;
+//    l->next = malloc(sizeof(Node));
+//    l->next->data = 66;
+//    l->next->next = NULL;
+//    for(Node* it = l; it; it=it->next){
+//        printf("%d\n", it->data);
+//    }
+//    free(l);
+//    l = NULL;
+
     return 0;
 }
